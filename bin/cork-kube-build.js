e967d4365d6f58fc77d919455f61668b5e230ac8
Justin Merz <jrmerz@gmail.com>
2024-11-13 15:54:24 -0800

    build bug fixes

--- bin/cork-kube-build.js
+++ bin/cork-kube-build.js
@@ -1,49 +1,12 @@
 import { Command } from 'commander';
-import init from '../lib/init-env.js';
 import config from '../lib/config.js';
 import build from '../lib/build.js';
 import buildDependencies from '../lib/build-dependencies.js';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-import kubectl from '../lib/kubectl.js';
 
 const program = new Command();
-
-// program
-//   .argument('<env>', 'environment to start')
-//   .requiredOption('-l, --location <location>', 'location of the build')
-//   .option('-c, --config <config>', 'path to config file')
-//   .option('-p, --project <project>', 'project name')
-//   .option('-i, --image <name>', 'only build a specific')
-//   // .option('-g, --group <name>', 'only build a specific group of images')
-//   .option('-r, --redeploy', 'redeploy service, deletes it first then deploys. A service must be specified')
-//   .option('-x, --no-docker-logs', 'do not show docker logs during build')
-//   .option('-d, --debug', 'debug/dry run build.  Just prints the docker build command')
-//   .action(async (env, opts) => {
-//     config.init(opts.config, opts);
-    
-//     if( !config.data.local.build ) {
-//       console.error(`No build configuration found in ${config.localFile}`);
-//       process.exit(1);
-//     }
-
-//     if( opts.image ) {
-//       let imageConfig = config.data.local.build.image.find(i => i.name == opts.image);
-//       if( !imageConfig ) {
-//         console.error(`Image ${opts.image} not found in .cork-kube-config build.image`);
-//         process.exit(1);
-//       }
-//       await build.buildImage(opts.image, env, opts.location, config.data.local.build || {}, opts);
-//       console.log('');
-//       return;
-//     }
-
-//     for( let image of config.data.local.build.images ) {
-//       await build.buildImage(image.name, env, opts.location, config.data.local.build || {}, opts);
-//     }
-
-//   });
 
 program
   .command('gcb')
@@ -139,52 +102,39 @@ program
     config.saveGlobal();
   });
 
-program.command('show')
-  .description('show build configuration')
-  .requiredOption('-p, --project <project>', 'project name')
-  .requiredOption('-v, --version <version>', 'version to build')
-  .option('-r, --use-remote <repoNameOrUrl>', 'use remote git repository instead of configured local directory')
-  .option('-o, --output <file>', 'output format, json, yaml, text. Default: text')
+program
+  .command('list')
+  .description('list all projects and their versions')
+  .option('-p, --project <project>', 'filter to a project name')
+  .option('-n, --names', 'just list project names')
   .action(async (opts) => {
-    if( opts.useRemote ) {
-      opts.useRemote = opts.useRemote.split(/(,| )/g)
-        .map(i => i.trim());
-    }
-    buildDependencies.load();
-    let graph = buildDependencies.getBuildGraph(opts.project, opts.version, opts);
-    if( opts.output == 'json' ) {
-      console.log(JSON.stringify(graph, null, 2));
-      return;
-    }
-    if( opts.output == 'yaml' ) {
-      console.log(yaml.dump(graph));
-      return;
-    } 
+    await buildDependencies.load();
 
-    let root = graph[opts.project];
+    let list = {};
+    for( let project in buildDependencies.dependencies ) {
+      if( opts.project && project !== opts.project ) {
+        continue;
+      }
 
-    console.log(`Project: ${opts.project}`);
-    if( root.localDir ) {
-      console.log(`Local Directory: ${root.localDir}`);
-    } else {
-      console.log(`Version: ${opts.version}`);
-      console.log(`Repository: ${root.url}`);
+      let info = buildDependencies.dependencies[project];
+      list[project] = {
+        url : info.repository,
+        versions : []
+      };
+     
+      if( opts.names ) continue;
+
+      for( let version in info.builds ) {
+        list[project].versions.push(version);
+      }
     }
-    console.log(`Dependencies:`);
-    printDependencies(root.dependencies);
+
+    if( opts.names ) {
+      list = Object.keys(list);
+    }
+
+    console.log(yaml.dump(list));
   });
-
-function printDependencies(deps, depth=1) {
-  for( let dep in deps ) {
-    let info = deps[dep];
-    if( info.localDir ) {
-      console.log(`${'  '.repeat(depth)}- ${dep} ${info.localDir}`);
-    } else {
-      console.log(`${'  '.repeat(depth)}- ${dep} ${info.url} v${info.version}`);
-    }
-    printDependencies(info.dependencies, depth+1);
-  }
-}
 
 program
   .command('show-local-repos')
